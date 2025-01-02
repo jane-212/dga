2b18aec2fdb16649189f4c912724b1dbf6a091ae
jane <jjt1525@outlook.com>
2025-01-02 20:11:00 +0800

    rebase

--- crates/dga/src/download/mod.rs
+++ crates/dga/src/download/mod.rs
@@ -335,9 +335,10 @@ impl Download {
                         _ => break,
                     }
 
-                cx.background_executor()
-                    .timer(Duration::from_millis(1_500))
-                    .await;
+                    cx.background_executor()
+                        .timer(Duration::from_millis(1_500))
+                        .await;
+                }
             }
         })
         .detach();
